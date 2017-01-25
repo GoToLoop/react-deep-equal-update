dafde9baedea9f09f5b5d655aac085e06554646c
GoToLoop <GoToLoop@users.noreply.github.com>
2017-01-25 15:19:34 -0200

    Refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,26 @@
 /// <reference path="./interfaces.d.ts"/>
 
-import * as deepEqual from "deep-equal";
+import * as deepEqual from 'deep-equal';
 
-export default function updateWhenNotDeepEqual(subprop?: (string|subprop), substate?: (string|subprop)) {
-    return function(target: any) {
+export default function updateWhenNotDeepEqual(subprop?: string | subprop, substate?: string | subprop): ClassDecorator {
+    return <TFn extends Function>(target: TFn) => {
         target.prototype.shouldComponentUpdate = function (nextProps: Object) {
+            let _nextProps = nextProps, _props = this.props;
 
-            let _nextProps = nextProps;
-            let _props = this.props;
-
-            if (typeof subprop !== "undefined") {
-
+            if (subprop) {
                 _nextProps = Object.assign({}, nextProps);
                 _props = Object.assign({}, this.props);
 
-                if (typeof subprop === "string") {
+                if (typeof subprop === 'string') {
                     _nextProps = _nextProps[subprop];
                     _props = _props[subprop];
-                }
-
-                if (typeof subprop === "function") {
+                } else {
                     _nextProps = subprop(_nextProps);
                     _props = subprop(_props);
                 }
-
             }
 
-            let updateByProps = !deepEqual(_nextProps, _props);
-            return updateByProps;
-
+            return !deepEqual(_nextProps, _props);
         };
-        return target;
     };
-};
+}
